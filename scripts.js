d4d7e7c6da1f0540cc7958e8cd390c569eb47180
ilkim berfin kayar <ilkimberfink@outlook.com>
2023-02-23 18:44:48 -0600

    First commit

--- scripts.js
+++ scripts.js
@@ -1,2 +1,13 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+document.addEventListener("DOMContentLoaded", function()){
+    const takeOff = document.getElementById("takeoff");
+    takeOff.addEventListener("click", function(event){
+        window.confirm("Confirm that the shuttle is ready for takeoff.");
+        document.getElementById("flightStatus") = "Shuttle in flight.";
+        event.target.style.shuttleBackground = "blue";
+        document.getElementById("spaceShuttleHeight") += 10000;
+    });
+
+
+}
